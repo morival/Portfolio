cc0e8c3623eba6264fc8094557f0e15b5bf1b256
morival <64006835+morival@users.noreply.github.com>
2021-06-05 03:41:41 +0100

    testing project containers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
-import Building from './components/Building';
+// import Building from './components/Building';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,15 @@ function About() {
             <h2>
                 About
             </h2>
-            <p>An enthusiastic and dedicated Software Developer with excellent interpersonal and problem-solving skills as well as great analytic and planning ability. 
-                My collaborative mindset and passion for coding makes me a reliable and adaptable teammate, who is keen to step out of his comfort zone and constantly keep learning. 
-                I have a strong interest in innovative software engineering projects that require creativity, attention to detail, and both abstract and strategic thinking.
-</p>
-        
+            <div className="flex">
+                <div>
+                    <p>An enthusiastic and dedicated Software Developer with excellent interpersonal and problem-solving skills as well as great analytic and planning ability. 
+                        My collaborative mindset and passion for coding makes me a reliable and adaptable teammate, who is keen to step out of his comfort zone and constantly keep learning. 
+                        I have a strong interest in innovative software engineering projects that require creativity, attention to detail, and both abstract and strategic thinking.
+                    </p>
+                </div>
+                <div></div>
+            </div>
         </section>
     )
 }
