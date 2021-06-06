e0bffa784022d1f01a68da9041835de2736af732
morival <64006835+morival@users.noreply.github.com>
2021-06-06 17:27:19 +0100

    added images for 2 projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './project.css';
+import project_01 from '../resources/project_01.jpg';
+import project_02 from '../resources/project_02.jpg';
 
 function Projects() {
 
@@ -7,32 +9,47 @@ function Projects() {
         <section id="projects">
             <h2>Projects</h2>
             <div className="container flex">
-                <div className="project-container flex">
-                    <div className="project-img"></div>
-                    <h3>My Destination | Travel Planner/Tracker | Solo Project</h3>
-                    <p>
-                        A full stack web application with CRUD functions created using Python. 
-                        The app enables users to plan, track and manage travel adventures. 
-                        Built over 5 days using Flask framework and SQL for the backend database.
-                    </p>
+                <div className="project-container">
+                    <div className="project-img">
+                        <img src={project_01} alt="project-1" />
+                    </div>
+                    <div className="project-info">
+                        <h3>My Destination | Travel Planner/Tracker | Solo Project</h3>
+                        <p>
+                            A full stack web application with CRUD functions created using Python. 
+                            The app enables users to plan, track and manage travel adventures. 
+                            Built over 5 days using Flask framework and SQL for the backend database.
+                        </p>
+                    </div>
+                    
                 </div>
-                <div className="project-container flex">
-                    <div className="project-img"></div>
-                    <h3>Cosmodex | Educational App | Group Project</h3>
-                    <p>
-                        An educational application built using the Vue.js framework. 
-                        Designed with schoolchildren in mind, it educates the user about the Solar System in a fun and interactive way. 
-                        The app renders components dynamically based on JSON format data fetched from external APIs and a MongoDB server.
-                    </p>
+                <div className="project-container">
+                    <div className="project-img">
+                        <img src={project_02} alt="project-2" />
+                    </div>
+                    <div className="project-info">
+                        <h3>Cosmodex | Educational App | Group Project</h3>
+                        <p>
+                            An educational application built using the Vue.js framework. 
+                            Designed with schoolchildren in mind, it educates the user about the Solar System in a fun and interactive way. 
+                            The app renders components dynamically based on JSON format data fetched from external APIs and a MongoDB server.
+                        </p>
+                    </div>
+                    
                 </div>
-                <div className="project-container flex">
-                    <div className="project-img"></div>
-                    <h3>Shop Local | eCommerce App | Group Project</h3>
-                    <p>
-                        An e-commerce application that provides commercial clients with a platform to promote their businesses and enables individual customers to find and book services available in their local area. 
-                        The application is built with responsive components and laid out using the React framework. 
-                        Backend written using Spring Data REST in Java.
-                    </p>
+                <div className="project-container">
+                    <div className="project-img">
+
+                    </div>
+                    <div className="project-info">
+                        <h3>Shop Local | eCommerce App | Group Project</h3>
+                        <p>
+                            An e-commerce application that provides commercial clients with a platform to promote their businesses and enables individual customers to find and book services available in their local area. 
+                            The application is built with responsive components and laid out using the React framework. 
+                            Backend written using Spring Data REST in Java.
+                        </p>
+                    </div>
+                    
                 </div>
             </div>
         </section>
