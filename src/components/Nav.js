8b8fca05edb7275703c6d9d5713fc68b97c1af96
morival <64006835+morival@users.noreply.github.com>
2021-06-30 01:04:04 +0100

    added home button to nav menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,8 @@ function Nav() {
 
     return(
         <section id="nav">
-            <div className="section-link"><Link activeClass="active" to="about" spy={true} smooth={true}>About</Link></div>
+            <div className="section-link"><Link to="home" spy={true} smooth={true}>Matt Harlos</Link></div>
+            <div className="section-link"><Link to="about" spy={true} smooth={true}>About</Link></div>
             <div className="section-link"><Link to="projects" spy={true} smooth={true}>Projects</Link></div>
             <div className="section-link"><Link to="contact" spy={true} smooth={true}>Contact</Link></div>
         
