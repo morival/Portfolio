6e9a3a6ce42aaf5f8e2dc2373a2ffb0ff6e98d56
morival <64006835+morival@users.noreply.github.com>
2022-08-02 14:34:56 +0100

    reorganising modal content

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,33 +1,9 @@
 import React from "react";
 import './modal.css';
+import ListOfProjects from "../content/ListOfProjects";
 
 function Modal({ modal, close }) {
 
-    const projects = [{
-        modalLabel: "My Destination",
-        modalHeader: "Flask (Python) / MySQL",
-        modalContent: "A full stack web application with CRUD functions created using Python.The app enables users to plan, track and manage travel adventures. Built over 5 days using Flask framework and SQL for the backend database.",
-        gitHub: "https://github.com/morival/W04_project_TBL"
-    },
-    {
-        modalLabel: "Cosmodex",
-        modalHeader: "Vue.js (JavaScript) / Express.js / MongoDB ",
-        modalContent: "An educational application built using the Vue.js framework. Designed with schoolchildren in mind, it educates the user about the Solar System in a fun and interactive way. The app renders components dynamically based on JSON format data fetched from external APIs and a MongoDB server.",
-        website: "https://morival.github.io/",
-        gitHub: "https://github.com/morival/morival.github.io"
-    },
-    {
-        modalLabel: "Shop Local",
-        modalHeader: "React.js (JavaScript) / Spring Boot (Java)",
-        modalContent: "An e-commerce application that provides commercial clients with a platform to promote their businesses and enables individual customers to find and book services available in their local area. The application is built with responsive components and laid out using the React framework. Backend written using Spring Data REST in Java.",
-        gitHub:"https://github.com/morival/final_project_frontend"
-    },
-    {
-        modalLabel: "Rickshaw Registry",
-        modalHeader: "React.js (JavaScript) / Express.js / MongoDB",
-        modalContent: "Application designed to help train new rickshaw riders as well as providing a maintenance checklist and reminders for licence and insurance renewal.",
-        gitHub: "https://github.com/morival/rickshaw-registry"
-    }];
     
     // Find index position of the Project from file number.
     const findProjectIndex = (modal) => {
@@ -40,7 +16,7 @@ function Modal({ modal, close }) {
         const contentIndex = (content) => {
             return content[findProjectIndex(modal)];
         };
-        const project = contentIndex(projects);
+        const project = contentIndex(ListOfProjects);
         return project;
     };
     // Select Label of the project
---
+++ src/content/ListOfProjects.js
@@ -0,0 +1,59 @@
+// import project_01 from '../resources/project_01.jpg';
+// import project_02 from '../resources/project_02.jpg';
+// import project_03 from '../resources/project_03.jpg';
+// import project_04 from '../resources/project_04.jpg';
+
+
+const ListOfProjects = [{
+    modalLabel: "My Destination",
+    modalHeader: "Flask (Python) / MySQL",
+    modalContent: "A full stack web application with CRUD functions created using Python.The app enables users to plan, track and manage travel adventures. Built over 5 days using Flask framework and SQL for the backend database.",
+    gitHub: "https://github.com/morival/W04_project_TBL"
+},
+{
+    modalLabel: "Cosmodex",
+    modalHeader: "Vue.js (JavaScript) / Express.js / MongoDB ",
+    modalContent: "An educational application built using the Vue.js framework. Designed with schoolchildren in mind, it educates the user about the Solar System in a fun and interactive way. The app renders components dynamically based on JSON format data fetched from external APIs and a MongoDB server.",
+    website: "https://morival.github.io/",
+    gitHub: "https://github.com/morival/morival.github.io"
+},
+{
+    modalLabel: "Shop Local",
+    modalHeader: "React.js (JavaScript) / Spring Boot (Java)",
+    modalContent: "An e-commerce application that provides commercial clients with a platform to promote their businesses and enables individual customers to find and book services available in their local area. The application is built with responsive components and laid out using the React framework. Backend written using Spring Data REST in Java.",
+    gitHub:"https://github.com/morival/final_project_frontend"
+},
+{
+    modalLabel: "Rickshaw Registry",
+    modalHeader: "React.js (JavaScript) / Express.js / MongoDB",
+    modalContent: "Application designed to help train new rickshaw riders as well as providing a maintenance checklist and reminders for licence and insurance renewal.",
+    gitHub: "https://github.com/morival/rickshaw-registry"
+}];
+
+// const projects = [{
+//     name: "Rickshaw Registry",
+//     tech: "React.js (JavaScript) / Express.js / MongoDB",
+//     setShow: setShow4,
+//     show: show4,
+//     img: project_04
+// },{
+//     name: "Shop Local",
+//     tech: "React.js (JavaScript) / Express.js / Spring Boot (Java)",
+//     setShow: setShow3,
+//     show: show3,
+//     img: project_03
+// },{
+//     name: "Cosmodex",
+//     tech: "Vue.js (JavaScript) / Express.js / MongoDB",
+//     setShow: setShow2,
+//     show: show2,
+//     img: project_02
+// },{
+//     name: "My Destination",
+//     tech: "Flask (Python) / MySQL",
+//     setShow: setShow1,
+//     show: show1,
+//     img: project_01
+// }]
+
+export default ListOfProjects;
